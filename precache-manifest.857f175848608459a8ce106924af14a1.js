c5b026bfb37f82209123fc755050dda3313e3ce9
clipwww <clipwww@users.noreply.github.com>
2020-11-12 10:31:44 +0000

    deploy: 20271afdee6116aad5b22905496b73dabc1aad14

---
+++ precache-manifest.857f175848608459a8ce106924af14a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c29008c2cc6756c95645ebd09479ce3",
+    "url": "/movie-record/404.html"
+  },
+  {
+    "revision": "2e2658fa3cfc08c9f676",
+    "url": "/movie-record/css/app.0bba6588.css"
+  },
+  {
+    "revision": "e59a5d130951f629326fd06cab4cf5a1",
+    "url": "/movie-record/index.html"
+  },
+  {
+    "revision": "2e2658fa3cfc08c9f676",
+    "url": "/movie-record/js/app.57d216e0.js"
+  },
+  {
+    "revision": "b7dbf0e40d0f049d08e0",
+    "url": "/movie-record/js/chunk-vendors.9e8c12f3.js"
+  },
+  {
+    "revision": "7d7a2dbd9dbc744efb18dc647dbd7792",
+    "url": "/movie-record/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/movie-record/robots.txt"
+  }
+]);
