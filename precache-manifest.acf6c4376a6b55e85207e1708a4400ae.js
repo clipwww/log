f49516c08c67bc2ae3569a4d2a8e279758483a1c
clipwww <clipwww@users.noreply.github.com>
2020-11-14 12:02:52 +0000

    deploy: bc94c3805b63f80b35431462e798a7e967332573

--- precache-manifest.acf6c4376a6b55e85207e1708a4400ae.js
+++ precache-manifest.acf6c4376a6b55e85207e1708a4400ae.js
@@ -4,20 +4,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-record/404.html"
   },
   {
-    "revision": "5ab7838b8848787b7fb9",
+    "revision": "70ccced6696af5021aa6",
     "url": "/movie-record/css/app.7bdf4b1e.css"
   },
   {
-    "revision": "88b58f00e72b0d87d53a610ea9d60bfb",
+    "revision": "1b21f3704eed8ae865ef415e5c01f00b",
     "url": "/movie-record/index.html"
   },
   {
-    "revision": "5ab7838b8848787b7fb9",
-    "url": "/movie-record/js/app.cd04d587.js"
+    "revision": "70ccced6696af5021aa6",
+    "url": "/movie-record/js/app.7c8ae01a.js"
   },
   {
-    "revision": "b08d0a454e3704861f39",
-    "url": "/movie-record/js/chunk-vendors.53942d4d.js"
+    "revision": "9590a2268509698a970a",
+    "url": "/movie-record/js/chunk-vendors.9dbffd8e.js"
   },
   {
     "revision": "7d7a2dbd9dbc744efb18dc647dbd7792",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-record/precache-manifest.0dbfed30c2e7c15f8baf81ec6bc71fe9.js"
+  "/movie-record/precache-manifest.acf6c4376a6b55e85207e1708a4400ae.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "movie-record"});
