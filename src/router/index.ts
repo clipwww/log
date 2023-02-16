ba146404d8b40972915fc746a5ec28b01e1a5e00
David, Jian <clipwww@gmail.com>
2023-02-16 15:47:32 +0800

    chore: upgrade node-18

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,26 +11,23 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/movie',
     name: 'Movie',
-    component: () => import(/* webpackChunkName: "movie" */ '../views/Movie.vue')
+    component: () => import('../views/Movie.vue')
   },
   {
     path: '/mi',
     name: 'Mi',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "mi" */ '../views/Mi.vue'),
+    component: () => import('../views/Mi.vue'),
     redirect: { name: 'MiSport' },
     children: [
       {
         path: 'sport',
         name: 'MiSport',
-        component: () => import(/* webpackChunkName: "mi-sport" */ '../views/MiSport.vue'),
+        component: () => import('../views/MiSport.vue'),
       },
       {
         path: 'activity',
         name: 'MiActivity',
-        component: () => import(/* webpackChunkName: "mi-sport" */ '../views/MiActivity.vue'),
+        component: () => import('../views/MiActivity.vue'),
       }
     ]
   }
